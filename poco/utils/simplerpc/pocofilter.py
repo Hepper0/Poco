b68cfda1e9424f235505d3101e3f8aba8665c808
Hepper0 <31428761+Hepper0@users.noreply.github.com>
2019-05-08 18:15:25 +0800

    Update pocofilter.py
    std_poco支持多线程

--- poco/utils/simplerpc/pocofilter.py
+++ poco/utils/simplerpc/pocofilter.py
@@ -1,14 +1,22 @@
+import threading
 
+local = threading.local()
+
+# class PocoFilter(object):
+#
+#     NodeType=None
+#     SubType= "*"
+#     Condition={}
+#
+#
+# def init_filter():
+#     PocoFilter.NodeType = None
+#     PocoFilter.SubType = "*"
+#     PocoFilter.Condition = {}
 
 class PocoFilter(object):
-
-    NodeType=None
-    SubType= "*"
-    Condition={}
-
-
-def init_filter():
-    PocoFilter.NodeType = None
-    PocoFilter.SubType = "*"
-    PocoFilter.Condition = {}
+    def __init__(self):
+        self.NodeType = None
+        self.SubType = "*"
+        self.Condition = {}
 
